4125017b414c51d71ed1e6b858bd26e94a5632f2
Egor <weqqu2014@yandex.ru>
2021-03-28 17:45:05 +0300

    Add implementation/handling for a few opcodes, complete memory map (#1)

--- instruction.go
+++ instruction.go
@@ -2,14 +2,22 @@ package main
 
 // See https://en.wikipedia.org/wiki/MIPS_architecture#Instruction_formats
 type Instruction struct {
+	// Opcode is primary opcode value
 	Opcode uint32
 
-	Rs, Rt, Rd  uint32
+	// Rs, Rt, Rd are register indices
+	Rs, Rt, Rd uint32
+	// ShiftAmount is offset value
 	ShiftAmount uint32
-	Function    uint32
+	// Function is secondary opcode value
+	Function uint32
 
-	Imm16, Imm16sx uint32
+	// Imm16 is immediate value extended to 32 bits
+	Imm16 uint32
+	// Imm16sx is sign extended immediate value
+	Imm16sx uint32
 
+	// Address is address value
 	Address uint32
 }
 
@@ -27,6 +35,6 @@ func NewInstruction(value uint32) Instruction {
 	}
 }
 
-func (insn *Instruction) String() string {
+func (inst *Instruction) String() string {
 	return "unimplemented"
 }
