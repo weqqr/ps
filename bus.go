0b8333ee974db0bddf2b2e1b52a4a466560feade
UnTea <weqqu2014@yandex.ru>
2021-03-28 17:30:12 +0300

    add implementation/handling for a few opcodes, completed memory map

--- bus.go
+++ bus.go
@@ -5,22 +5,52 @@ import (
 )
 
 // Memory map
+// http://problemkaputt.de/psx-spx.htm#memorymap
 const (
-	BiosStart = 0x1FC00000
-	BiosSize  = 512 * 1024
+	MainRAM               = 0x00000000
+	FirstExpansionRegion  = 0x1F000000
+	Scratchpad            = 0x1F800000
+	IOPorts               = 0x1F801000
+	SecondExpansionRegion = 0x1F801000
+	ThirdExpansionRegion  = 0x1F802000
+	BIOSAddress           = 0x1FC00000
+	CacheControl          = 0xFFFE0000
+
+	MainRAMSize               = 2048 * 1024
+	FirstExpansionRegionSize  = 8192 * 1024
+	ScratchpadSize            = 1 * 1024
+	IOPortsSize               = 8 * 1024
+	SecondExpansionRegionSize = 8 * 1024
+	ThirdExpansionRegionSize  = 2048 * 1024
+	BIOSSize                  = 512 * 1024
+	CacheControlSize          = 512
 )
 
 type Bus struct {
-	bios []byte
+	mainRAM               []byte
+	firstExpansionRegion  []byte
+	scratchpad            []byte
+	ioPorts               []byte
+	secondExpansionRegion []byte
+	thirdExpansionRegion  []byte
+	bios                  []byte
+	cacheControl          []byte
 }
 
 func NewBus(bios []byte) Bus {
-	if len(bios) != 512*1024 {
-		log.Fatal("Error: BIOS size must be exactly 512 KiB")
+	if len(bios) != BIOSSize {
+		log.Fatal("Error: BIOSAddress size must be exactly 512 KiB")
 	}
 
 	return Bus{
-		bios: bios,
+		mainRAM:               make([]byte, MainRAMSize),
+		firstExpansionRegion:  make([]byte, FirstExpansionRegionSize),
+		scratchpad:            make([]byte, ScratchpadSize),
+		ioPorts:               make([]byte, IOPortsSize),
+		secondExpansionRegion: make([]byte, SecondExpansionRegionSize),
+		thirdExpansionRegion:  make([]byte, ThirdExpansionRegionSize),
+		bios:                  bios,
+		cacheControl:          make([]byte, CacheControlSize),
 	}
 }
 
@@ -29,12 +59,28 @@ func inRange(value, start, size uint32) bool {
 }
 
 func (bus *Bus) Map(address uint32) (uint32, []byte) {
+	if inRange(address, CacheControl, CacheControlSize) {
+		return address - CacheControl, bus.cacheControl
+	}
+
 	// Mask segment
 	address = address & 0x1FFFFFFF
 
 	switch {
-	case inRange(address, BiosStart, BiosSize):
-		return address - BiosStart, bus.bios
+	case inRange(address, MainRAM, MainRAMSize):
+		return address - MainRAM, bus.mainRAM
+	case inRange(address, FirstExpansionRegion, FirstExpansionRegionSize):
+		return address - FirstExpansionRegion, bus.firstExpansionRegion
+	case inRange(address, Scratchpad, ScratchpadSize):
+		return address - Scratchpad, bus.scratchpad
+	case inRange(address, IOPorts, IOPortsSize):
+		return address - IOPorts, bus.ioPorts
+	case inRange(address, SecondExpansionRegion, SecondExpansionRegionSize):
+		return address - SecondExpansionRegion, bus.secondExpansionRegion
+	case inRange(address, ThirdExpansionRegion, ThirdExpansionRegionSize):
+		return address - ThirdExpansionRegion, bus.thirdExpansionRegion
+	case inRange(address, BIOSAddress, BIOSSize):
+		return address - BIOSAddress, bus.bios
 	default:
 		log.Fatalf("unknown memory region at address %x", address)
 	}
@@ -78,7 +124,7 @@ func (bus *Bus) StoreWord(address uint32, value uint32) {
 	address, data := bus.Map(address)
 
 	data[address+3] = uint8(value)
-	data[address+2] = uint8(value)
-	data[address+1] = uint8(value)
-	data[address] = uint8(value >> 8)
+	data[address+2] = uint8(value << 8)
+	data[address+1] = uint8(value << 16)
+	data[address] = uint8(value << 24)
 }
