4c9c958a0215031fb6de5ac3dc2d0d4c1296f24d
UnTea <weqqu2014@yandex.ru>
2021-03-31 00:54:34 +0300

    added new instructions, fixed old instructions

--- ps/cpu.go
+++ ps/cpu.go
@@ -23,99 +23,127 @@ func NewCPU() CPU {
 	}
 }
 
-func (cpu *CPU) LUI(instruction Instruction, bus *Bus) {
-	cpu.GPR[instruction.Rt] = instruction.Imm16 << 16
-}
-
-func (cpu *CPU) ORI(instruction Instruction, bus *Bus) {
-	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] | instruction.Imm16
-}
-
-func (cpu *CPU) SW(instruction Instruction, bus *Bus) {
-	address := instruction.Imm16 | cpu.GPR[instruction.Rs]
-	bus.StoreWord(address, cpu.GPR[instruction.Rt])
-}
-
 func (cpu *CPU) SLL(instruction Instruction, bus *Bus) {
 	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rt] << instruction.ShiftAmount
 }
 
-func (cpu *CPU) ADDIU(instruction Instruction, bus *Bus) {
-	//TODO 32-bit-overflow
-	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] + instruction.Imm16sx
+func (cpu *CPU) SRL(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rt] >> instruction.ShiftAmount
 }
 
-func (cpu *CPU) J(instruction Instruction, bus *Bus) {
-	cpu.PcNext = cpu.Pc&0xF0000000 | (instruction.Address << 2)
+func (cpu *CPU) SRA(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = uint32(int32(cpu.GPR[instruction.Rt]) << instruction.ShiftAmount)
 }
 
-func (cpu *CPU) MFC(instruction Instruction, bus *Bus) {
-	//TODO Coprocessor
+func (cpu *CPU) SLLV(instruction Instruction, bus *Bus) {
+	s := cpu.GPR[instruction.Rs] & 0x1F
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rt] << s
 }
 
-func (cpu *CPU) CFC(instruction Instruction, bus *Bus) {
-	//TODO Coprocessor
+func (cpu *CPU) SRLV(instruction Instruction, bus *Bus) {
+	s := cpu.GPR[instruction.Rs] & 0x1F
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rt] >> s
 }
 
-func (cpu *CPU) MTC(instruction Instruction, bus *Bus) {
-	//TODO Coprocessor
+func (cpu *CPU) SRAV(instruction Instruction, bus *Bus) {
+	s := cpu.GPR[instruction.Rs] & 0x1F
+	cpu.GPR[instruction.Rd] = uint32(int32(cpu.GPR[instruction.Rt]) >> s)
 }
 
-func (cpu *CPU) CTC(instruction Instruction, bus *Bus) {
-	//TODO Coprocessor
+func (cpu *CPU) JR(instruction Instruction, bus *Bus) {
+	cpu.PcNext = cpu.GPR[instruction.Rs]
 }
 
-func (cpu *CPU) BNE(instruction Instruction, bus *Bus) {
-	instruction.Address = instruction.Imm16sx << 2
-	if cpu.GPR[instruction.Rs] != cpu.GPR[instruction.Rt] {
-		cpu.PcNext += instruction.Address
+func (cpu *CPU) JALR(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.Pc + 8
+	cpu.PcNext = cpu.GPR[instruction.Rs]
+}
+
+func (cpu *CPU) SYSCALL(instruction Instruction, bus *Bus) {
+	//TODO SystemCallException
+}
+
+func (cpu *CPU) BREAK(instruction Instruction, bus *Bus) {
+	//TODO BreakpointException
+}
+
+func (cpu *CPU) MFHI(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) MTHI(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) MFLO(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) MTLO(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) MULT(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) MULTU(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) DIV(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) DIVU(instruction Instruction, bus *Bus) {
+	//TODO implementation of the quotient and remainder
+}
+
+func (cpu *CPU) ADD(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = uint32(int32(cpu.GPR[instruction.Rs]) + int32(cpu.GPR[instruction.Rt]))
+}
+
+func (cpu *CPU) ADDU(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rs] + cpu.GPR[instruction.Rt]
+}
+
+func (cpu *CPU) SUB(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = uint32(int32(cpu.GPR[instruction.Rs]) - int32(cpu.GPR[instruction.Rt]))
+}
+
+func (cpu *CPU) SUBU(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rs] - cpu.GPR[instruction.Rt]
+}
+
+func (cpu *CPU) AND(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rs] & cpu.GPR[instruction.Rt]
+}
+
+func (cpu *CPU) OR(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rs] | cpu.GPR[instruction.Rt]
+}
+
+func (cpu *CPU) XOR(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rs] ^ cpu.GPR[instruction.Rt]
+}
+
+func (cpu *CPU) NOR(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rd] = ^(cpu.GPR[instruction.Rs] | cpu.GPR[instruction.Rt])
+}
+
+func (cpu *CPU) SLT(instruction Instruction, bus *Bus) {
+	if cpu.GPR[instruction.Rs] < cpu.GPR[instruction.Rt] {
+		cpu.GPR[instruction.Rd] = 1
+	} else {
+		cpu.GPR[instruction.Rd] = 0
 	}
 }
 
-func (cpu *CPU) ADDI(instruction Instruction, bus *Bus) {
-	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] + instruction.Imm16
-}
-
-func (cpu *CPU) LW(instruction Instruction, bus *Bus) {
-	//TODO Later
-}
-
-func (cpu *CPU) SH(instruction Instruction, bus *Bus) {
-	//TODO Later
-}
-
-func (cpu *CPU) JAL(instruction Instruction, bus *Bus) {
-	cpu.GPR[31] = cpu.Pc + 8
-	cpu.PcNext = cpu.Pc&0xF0000000 | (instruction.Address << 2)
-}
-
-func (cpu *CPU) LB(instruction Instruction, bus *Bus) {
-	//TODO Later
-}
-
-func (cpu *CPU) BEQ(instruction Instruction, bus *Bus) {
-	instruction.Address = instruction.Imm16sx << 2
-	if cpu.GPR[instruction.Rs] == cpu.GPR[instruction.Rt] {
-		cpu.PcNext += instruction.Address
-	}
-}
-
-func (cpu *CPU) SB(instruction Instruction, bus *Bus) {
-	//TODO Later
-}
-
-func (cpu *CPU) LBU(instruction Instruction, bus *Bus) {
-	//TODO Later
-}
-
-func (cpu *CPU) ANDI(instruction Instruction, bus *Bus) {
-	cpu.GPR[instruction.Rt] = instruction.Imm16 & cpu.GPR[instruction.Rs]
-}
-
-func (cpu *CPU) BLEZ(instruction Instruction, bus *Bus) {
-	instruction.Address = instruction.Imm16 << 2
-	if (cpu.GPR[instruction.Rs] >> 31) == 1 || cpu.GPR[instruction.Rs] == 0 {
-		cpu.PcNext += instruction.Address
+func (cpu *CPU) SLTU(instruction Instruction, bus *Bus) {
+	if (cpu.GPR[instruction.Rs] >> 1) < (cpu.GPR[instruction.Rt] >> 1) {
+		cpu.GPR[instruction.Rd] = 1
+	} else {
+		cpu.GPR[instruction.Rd] = 0
 	}
 }
 
@@ -135,20 +163,64 @@ func (cpu *CPU) BGEZ(instruction Instruction, bus *Bus) {
 
 func (cpu *CPU) BLTZAL(instruction Instruction, bus *Bus) {
 	instruction.Address = instruction.Imm16 << 2
-	condition := (cpu.GPR[instruction.Rs] >> 31) == 1
-	cpu.GPR[31] = cpu.Pc + 8
-	if condition {
+	if (cpu.GPR[instruction.Rs] >> 31) == 1 {
 		cpu.PcNext += instruction.Address
 	}
+	cpu.GPR[31] = cpu.Pc + 8
 }
 
 func (cpu *CPU) BGEZAL(instruction Instruction, bus *Bus) {
 	instruction.Address = instruction.Imm16 << 2
-	condition := (cpu.GPR[instruction.Rs] >> 31) == 0
-	cpu.GPR[31] = cpu.Pc + 8
-	if condition {
+	if (cpu.GPR[instruction.Rs] >> 31) == 0 {
 		cpu.PcNext += instruction.Address
 	}
+	cpu.GPR[31] = cpu.Pc + 8
+}
+
+func (cpu *CPU) J(instruction Instruction, bus *Bus) {
+	cpu.PcNext = cpu.Pc&0xF0000000 | (instruction.Address << 2)
+}
+
+func (cpu *CPU) JAL(instruction Instruction, bus *Bus) {
+	cpu.GPR[31] = cpu.Pc + 8
+	cpu.PcNext = cpu.Pc&0xF0000000 | (instruction.Address << 2)
+}
+
+func (cpu *CPU) BEQ(instruction Instruction, bus *Bus) {
+	instruction.Address = instruction.Imm16sx << 2
+	if cpu.GPR[instruction.Rs] == cpu.GPR[instruction.Rt] {
+		cpu.PcNext += instruction.Address
+	}
+}
+
+func (cpu *CPU) BNE(instruction Instruction, bus *Bus) {
+	instruction.Address = instruction.Imm16sx << 2
+	if cpu.GPR[instruction.Rs] != cpu.GPR[instruction.Rt] {
+		cpu.PcNext += instruction.Address
+	}
+}
+
+func (cpu *CPU) BLEZ(instruction Instruction, bus *Bus) {
+	instruction.Address = instruction.Imm16sx << 2
+	if (cpu.GPR[instruction.Rs]>>31) == 1 || cpu.GPR[instruction.Rs] == 0 {
+		cpu.PcNext += instruction.Address
+	}
+}
+
+func (cpu *CPU) BGTZ(instruction Instruction, bus *Bus) {
+	instruction.Address = instruction.Imm16sx << 2
+	if (cpu.GPR[instruction.Rs]>>31) == 0 && cpu.GPR[instruction.Rs] != 0 {
+		cpu.PcNext += instruction.Address
+	}
+}
+
+func (cpu *CPU) ADDI(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] + instruction.Imm16
+}
+
+func (cpu *CPU) ADDIU(instruction Instruction, bus *Bus) {
+	//TODO 32-bit-overflow
+	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] + instruction.Imm16sx
 }
 
 func (cpu *CPU) SLTI(instruction Instruction, bus *Bus) {
@@ -159,38 +231,92 @@ func (cpu *CPU) SLTI(instruction Instruction, bus *Bus) {
 	}
 }
 
-func (cpu *CPU) BGTZ(instruction Instruction, bus *Bus) {
-	instruction.Address = instruction.Imm16 << 2
-	if (cpu.GPR[instruction.Rs] >> 31) == 0 && cpu.GPR[instruction.Rs] != 0 {
-		cpu.PcNext += instruction.Address
-	}
-}
-
-func (cpu *CPU) LHU(instruction Instruction, bus *Bus) {
-	//TODO Later
-}
-
-func (cpu *CPU) XORI(instruction Instruction, bus *Bus) {
-	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] ^ instruction.Imm16
-}
-
-func (cpu *CPU) OR(instruction Instruction, bus *Bus) {
-	cpu.GPR[instruction.Rd] = cpu.GPR[instruction.Rs] | cpu.GPR[instruction.Rt]
-}
-
 func (cpu *CPU) SLTIU(instruction Instruction, bus *Bus) {
-	if (cpu.GPR[instruction.Rs] >> 1) < (instruction.Imm16 << 2) {
+	if (cpu.GPR[instruction.Rs] >> 1) < instruction.Imm16 {
 		cpu.GPR[instruction.Rd] = 1
 	} else {
 		cpu.GPR[instruction.Rd] = 0
 	}
 }
 
+func (cpu *CPU) ANDI(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rt] = instruction.Imm16 & cpu.GPR[instruction.Rs]
+}
+
+func (cpu *CPU) ORI(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] | instruction.Imm16
+}
+
+func (cpu *CPU) XORI(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rt] = cpu.GPR[instruction.Rs] ^ instruction.Imm16
+}
+
+func (cpu *CPU) LUI(instruction Instruction, bus *Bus) {
+	cpu.GPR[instruction.Rt] = instruction.Imm16 << 16
+}
+
+func (cpu *CPU) MFC(instruction Instruction, bus *Bus) {
+	//TODO Coprocessor
+}
+
+func (cpu *CPU) CFC(instruction Instruction, bus *Bus) {
+	//TODO Coprocessor
+}
+
+func (cpu *CPU) MTC(instruction Instruction, bus *Bus) {
+	//TODO Coprocessor
+}
+
+func (cpu *CPU) CTC(instruction Instruction, bus *Bus) {
+	//TODO Coprocessor
+}
+
+func (cpu *CPU) LB(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
 func (cpu *CPU) LH(instruction Instruction, bus *Bus) {
 	//TODO Later
 }
 
+func (cpu *CPU) LW(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) LWL(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) LBU(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) LHU(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
 func (cpu *CPU) LWR(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) SB(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) SH(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) SWL(instruction Instruction, bus *Bus) {
+	//TODO Later
+}
+
+func (cpu *CPU) SW(instruction Instruction, bus *Bus) {
+	address := instruction.Imm16 + cpu.GPR[instruction.Rs]
+	bus.StoreWord(address, cpu.GPR[instruction.Rt])
+}
+
+func (cpu *CPU) SWR(instruction Instruction, bus *Bus) {
 	//TODO Later
 }
 
@@ -200,8 +326,60 @@ func (cpu *CPU) Execute(instruction Instruction, bus *Bus) {
 		switch instruction.Function {
 		case 0x00:
 			cpu.SLL(instruction, bus)
+		case 0x02:
+			cpu.SRL(instruction, bus)
+		case 0x03:
+			cpu.SRA(instruction, bus)
+		case 0x04:
+			cpu.SLLV(instruction, bus)
+		case 0x06:
+			cpu.SRLV(instruction, bus)
+		case 0x07:
+			cpu.SRAV(instruction, bus)
+		case 0x08:
+			cpu.JR(instruction, bus)
+		case 0x09:
+			cpu.JALR(instruction, bus)
+		case 0x0C:
+			cpu.SYSCALL(instruction, bus)
+		case 0x0D:
+			cpu.BREAK(instruction, bus)
+		case 0x10:
+			cpu.MFHI(instruction, bus)
+		case 0x11:
+			cpu.MTHI(instruction, bus)
+		case 0x12:
+			cpu.MFLO(instruction, bus)
+		case 0x13:
+			cpu.MTLO(instruction, bus)
+		case 0x18:
+			cpu.MULT(instruction, bus)
+		case 0x19:
+			cpu.MULTU(instruction, bus)
+		case 0x1A:
+			cpu.DIV(instruction, bus)
+		case 0x1B:
+			cpu.DIVU(instruction, bus)
+		case 0x20:
+			cpu.ADD(instruction, bus)
+		case 0x21:
+			cpu.ADDU(instruction, bus)
+		case 0x22:
+			cpu.SUB(instruction, bus)
+		case 0x23:
+			cpu.SUBU(instruction, bus)
+		case 0x24:
+			cpu.AND(instruction, bus)
 		case 0x25:
 			cpu.OR(instruction, bus)
+		case 0x26:
+			cpu.XOR(instruction, bus)
+		case 0x27:
+			cpu.NOR(instruction, bus)
+		case 0x2A:
+			cpu.SLT(instruction, bus)
+		case 0x2B:
+			cpu.SLTU(instruction, bus)
 		default:
 			log.Fatalf("unknown special instruction: %02x", instruction.Function)
 		}
@@ -265,6 +443,8 @@ func (cpu *CPU) Execute(instruction Instruction, bus *Bus) {
 		cpu.LH(instruction, bus)
 	case 0x23:
 		cpu.LW(instruction, bus)
+	case 0x22:
+		cpu.LWL(instruction, bus)
 	case 0x24:
 		cpu.LBU(instruction, bus)
 	case 0x25:
@@ -275,8 +455,12 @@ func (cpu *CPU) Execute(instruction Instruction, bus *Bus) {
 		cpu.SB(instruction, bus)
 	case 0x29:
 		cpu.SH(instruction, bus)
+	case 0x2A:
+		cpu.SWL(instruction, bus)
 	case 0x2B:
 		cpu.SW(instruction, bus)
+	case 0x2E:
+		cpu.SWR(instruction, bus)
 	default:
 		log.Fatalf("unknown primary instruction: %02x", instruction.Opcode)
 	}
